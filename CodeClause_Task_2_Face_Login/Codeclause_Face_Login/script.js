990213d505bff37eb3180617b9c781e22252007b
Rudra Prasad Mangaraj <130144848+rudra-prasad@users.noreply.github.com>
2023-05-30 16:28:41 +0530

    Add files via upload

---
+++ CodeClause_Task_2_Face_Login/Codeclause_Face_Login/script.js
@@ -0,0 +1,16 @@
+var video = document.getElementById('video');
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    var canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+});
